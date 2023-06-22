b18e1f2c5d4c9474b1cf50679b87312b95d670a1
behong <sijm@naver.com>
2023-06-22 23:38:50 +0900

    에니메이션

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
 
-  <Modal v-bind:원룸s="원룸s" :누른거="누른거" :모달창열렸니="모달창열렸니"/>
-
+  <transition name="fade">
+    <Modal @closeModal="모달창열렸니 = false" v-bind:원룸s="원룸s" :누른거="누른거" :모달창열렸니="모달창열렸니"/>
+  </transition>
+  
   <MenuOne/>
   
   <Discount />
+
+  <button @click="priceSort">가격순 정렬</button>
+
   <!--커스텀 이벤트-->
   <Card @openModal="모달창열렸니 = true; 누른거 =$event" :상품="원룸s[i]" v-for="(작명,i) in 원룸s" :key="작명" />
 
@@ -32,6 +37,11 @@ export default {
   methods : {
     increase(susu){
       this.신고수[susu] += 1;
+    },
+    priceSort(){
+      this.원룸s.sort(function(a,b){
+        return a.price - b.price
+      })
     }
   },
   components: {
@@ -44,6 +54,33 @@ export default {
 </script>
 
 <style>
+
+/* 퇴장 시작(에니메이션) */
+.fade-leave-from{
+  opacity: 1;
+}
+.fade-leave-active{
+  transition: all 1s;
+}
+
+/* 퇴장 끝 (에니메이션) */
+.fade-leave-to{
+  opacity: 0;
+}
+
+
+/* 시작(에니메이션) */
+.fade-enter-from{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition: all 1s;
+}
+
+/* 끝 (에니메이션) */
+.fade-enter-to{
+  opacity: 1;
+}
 
 
 #app {
@@ -59,5 +96,13 @@ export default {
   margin-top: 40px;
 }
 
+.start{
+  opacity: 0;
+  transition: all 1s;
+}
+
+.end{
+  opacity: 1;
+}
 
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,7 +4,19 @@
       <img :src="원룸s[누른거].image" style="width: 100%;">
       <h4>{{원룸s[누른거].title}}</h4>
       <p>{{원룸s[누른거].content}}</p>
-      <p>{{원룸s[누른거].price}} 원</p>
+      <!-- <input @input="month = $event.target.value"> -->
+      <input v-model="month">
+      <!-- <textarea v-model="month"></textarea>
+      <select v-model="month">
+        <option>123</option>
+        <option>2123</option>
+        <option>3123</option>
+      </select>
+      <input type="checkbox" v-model="month"> -->
+
+      <p> {{ month }}개월 선택함 : {{원룸s[누른거].price * month}} 원</p>
+      
+      <button @click="$emit('closeModal')">닫기</button>
       <!-- <button @click="모달창열렸니= false">닫기</button> -->
     </div>
   </div>
@@ -20,8 +32,20 @@ export default {
         누른거: Number,
         모달창열렸니: Boolean,
     },
+    watch:{
+      month(a){
+        //입력한 데이터가 13보다 크면 경고문
+        if( a > 13 ){
+          alert('13 세 이상 만..')
+        }
+        //month 가 변경 될때 마다 동작
+        // 사용자가 month 를 글자로 입력하면  경고문 
+
+      }
+    },
     data(){
         return{
+          month: '',
         }
   },    
 }
